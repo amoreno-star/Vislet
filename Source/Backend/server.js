f00283b2fedf069a141e2b8e1741f42bab50836c
amoreno-star <amoreno@urbanarts.org>
2026-07-13 11:34:01 -0400

    Pain in my ahhhhhhhhh

---
+++ Source/Backend/server.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCFAR1ndU7YMNbXCE22ZUAEsG9a3InIa6c",
+  authDomain: "vislet-8830b.firebaseapp.com",
+  projectId: "vislet-8830b",
+  storageBucket: "vislet-8830b.firebasestorage.app",
+  messagingSenderId: "730732470355",
+  appId: "1:730732470355:web:9e3f6afeb7930ff56266a6",
+  measurementId: "G-NB0MCDKXNP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- Source/Frontend/MainPage/index.js
+++
@@ -1,5 +0,0 @@
-const getStartedBtn = document.getElementById('getStartedButton');
-
-getStartedBtn.addEventListener('click', () => {
-    window.location.href = '../Dashboard/dashboard.html';
-});
